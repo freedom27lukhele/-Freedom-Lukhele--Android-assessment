6fd09fce9ddc4029a021e62835d72b22e8518efd
FreedomL <freedom27lukhele@gmail.com>
2025-03-02 23:10:28 +0200

    last set of instructions for my test

--- app/src/test/java/com/glucode/about_you/SortingTest.kt
+++ app/src/test/java/com/glucode/about_you/SortingTest.kt
@@ -1,70 +1,86 @@
 package com.glucode.about_you
 
 import android.view.MenuItem
-import androidx.test.core.app.ApplicationProvider
-import com.glucode.about_you.R
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.glucode.about_you.engineers.EngineersFragment
 import com.glucode.about_you.engineers.models.Engineer
 import com.glucode.about_you.engineers.models.QuickStats
+import com.glucode.about_you.R
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mockito.*
 
+@RunWith(AndroidJUnit4::class)
 class EngineersFragmentTest {
 
     private lateinit var fragment: EngineersFragment
     private lateinit var mockEngineers: List<Engineer>
-
     @Before
     fun setUp() {
+
         fragment = EngineersFragment()
-
-        mockEngineers = listOf(
-            Engineer("Freedom", "Android Developer", "", QuickStats(12,222,322),),  // 5 years
-            Engineer("Freedom", "Typescript Developer", "", QuickStats(10,234,222),),  // 5 years
-            Engineer("Freedom", "JAva Developer", "", QuickStats(20,233,233),),  // 5 years
-             )
-
-        fragment.setUpEngineersList(mockEngineers)
+        mockEngineers = mutableListOf(
+            Engineer("freedom", "Android Developer", "", QuickStats(12, 222, 322)),
+            Engineer("manqoba", "Typescript Developer", "", QuickStats(10, 234, 222)),
+            Engineer("sbu", "Java Developer", "", QuickStats(20, 233, 233)),
+        )
     }
 
     @Test
     fun `sorting engineers by years should be in ascending order`() {
-        val menuItem = mock(MenuItem::class.java)
-        `when`(menuItem.itemId).thenReturn(R.id.action_years)
+        val scenario = launchFragmentInContainer<EngineersFragment>()
+        scenario.moveToState(Lifecycle.State.RESUMED)
 
-        fragment.onOptionsItemSelected(menuItem)
+        scenario.onFragment { fragment ->
+            val menuItem = mock(MenuItem::class.java)
+            `when`(menuItem.itemId).thenReturn(R.id.action_years)
 
-        val sortedList = mockEngineers.sortedBy { it.quickStats.years }
-        assertEquals(sortedList[0].name, "freedom")
-        assertEquals(sortedList[1].name, "manqoba")
-        assertEquals(sortedList[2].name, "sbu")
+            fragment.onOptionsItemSelected(menuItem)
+
+            val sortedList = mockEngineers.sortedBy { it.quickStats.years }
+            assertEquals(sortedList[0].name, "manqoba")
+            assertEquals(sortedList[1].name, "freedom")
+            assertEquals(sortedList[2].name, "sbu")
+        }
     }
 
     @Test
     fun `sorting engineers by coffees should be in ascending order`() {
-        val menuItem = mock(MenuItem::class.java)
-        `when`(menuItem.itemId).thenReturn(R.id.action_coffees)
+        val scenario = launchFragmentInContainer<EngineersFragment>()
+        scenario.moveToState(Lifecycle.State.RESUMED)
 
-        fragment.onOptionsItemSelected(menuItem)
+        scenario.onFragment { fragment ->
+            val menuItem = mock(MenuItem::class.java)
+            `when`(menuItem.itemId).thenReturn(R.id.action_coffees)
 
-        val sortedList = mockEngineers.sortedBy { it.quickStats.coffees }
-        assertEquals(sortedList[0].name, "freedom")
-        assertEquals(sortedList[1].name, "manqoba")
-        assertEquals(sortedList[2].name, "sbu")
+            fragment.onOptionsItemSelected(menuItem)
+
+            val sortedList = mockEngineers.sortedBy { it.quickStats.coffees }
+            assertEquals(sortedList[0].name, "freedom")
+            assertEquals(sortedList[1].name, "manqoba")
+            assertEquals(sortedList[2].name, "sbu")
+        }
     }
 
     @Test
     fun `sorting engineers by bugs should be in ascending order`() {
-        val menuItem = mock(MenuItem::class.java)
-        `when`(menuItem.itemId).thenReturn(R.id.action_bugs)
+        val scenario = launchFragmentInContainer<EngineersFragment>()
+        scenario.moveToState(Lifecycle.State.RESUMED)
 
-        fragment.onOptionsItemSelected(menuItem)
+        scenario.onFragment { fragment ->
+            val menuItem = mock(MenuItem::class.java)
+            `when`(menuItem.itemId).thenReturn(R.id.action_bugs)
 
-        val sortedList = mockEngineers.sortedBy { it.quickStats.bugs }
-        assertEquals(sortedList[0].name, "sbu")
-        assertEquals(sortedList[1].name, "manqoba")
-        assertEquals(sortedList[2].name, "freedom")
+            fragment.onOptionsItemSelected(menuItem)
+
+            val sortedList = mockEngineers.sortedBy { it.quickStats.bugs }
+            assertEquals(sortedList[0].name, "sbu")
+            assertEquals(sortedList[1].name, "manqoba")
+            assertEquals(sortedList[2].name, "freedom")
+        }
     }
 }
